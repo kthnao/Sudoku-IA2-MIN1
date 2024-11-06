dd8505617f8f8818de9b92fe70844b0712339264
KyoTensu <loloduvaucluse@gmail.com>
2024-11-06 16:25:11 +0100

    Ajout de la classe du solveur graphique probabiliste

---
+++ Sudoku.GraphProba/GraphProba.cs
@@ -0,0 +1,11 @@
+﻿using Sudoku.Shared;
+
+namespace Sudoku.GraphProba;
+
+public class GraphProba : ISudokuSolver
+{
+    public SudokuGrid Solve(SudokuGrid s)
+    {
+        return s;
+    }
+}
---
+++ Sudoku.TestSolveur/SolverTest.cs
@@ -0,0 +1,12 @@
+﻿using Sudoku.Shared;
+
+namespace Sudoku.TestSolveur;
+
+public class SolverTest : ISudokuSolver
+{
+    public SudokuGrid Solve(SudokuGrid s)
+    {
+        return s;
+    }
+}
+
