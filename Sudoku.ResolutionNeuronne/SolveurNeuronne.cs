2038052c2880bb312c84a23ab29c86abeb291b22
EtienneGGG27 <etienne.gauge@laposte.net>
2024-11-06 16:26:28 +0100

    V init

---
+++ Sudoku.ResolutionNeuronne/SolveurNeuronne.cs
@@ -0,0 +1,12 @@
+﻿using Sudoku.Shared;
+
+namespace Sudoku.ResolutionNeuronne;
+
+public class SolveurNeuronne : ISudokuSolver 
+{
+    public SudokuGrid Solve(SudokuGrid s)
+    {
+        return s;
+    }
+
+}
