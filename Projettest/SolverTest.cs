ba845262c7ae674172532b159b6f07eda73c0559
AlexisP <alexis.pouillieute@gmail.com>
2024-11-06 17:22:15 +0100

    Add Neural Network solver implementation

---
+++ Projettest/SolverTest.cs
@@ -0,0 +1,12 @@
+using System;
+using Sudoku.Shared;
+
+namespace Projettest;
+
+public class SolverTest : ISudokuSolver
+{
+    public SudokuGrid Solve(SudokuGrid s)
+    {
+        return s;
+    }
+}
