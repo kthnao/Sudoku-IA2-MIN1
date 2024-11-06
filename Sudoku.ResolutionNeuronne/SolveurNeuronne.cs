73f2bf2ee91637a081099d0a2165dd5a5735f03b
EtienneGGG27 <etienne.gauge@laposte.net>
2024-11-06 17:11:55 +0100

    V init Python

--- Sudoku.ResolutionNeuronne/SolveurNeuronne.cs
+++ Sudoku.ResolutionNeuronne/SolveurNeuronne.cs
@@ -1,4 +1,5 @@
-﻿using Sudoku.Shared;
+﻿using Python.Runtime;
+using Sudoku.Shared;
 
 namespace Sudoku.ResolutionNeuronne;
 
@@ -8,5 +9,37 @@ public class SolveurNeuronne : ISudokuSolver
     {
         return s;
     }
+
+}
+
+
+public class SolveurNeuronnePython : PythonSolverBase
+{
+
+	public override Shared.SudokuGrid Solve(Shared.SudokuGrid s)
+	{
+		//System.Diagnostics.Debugger.Break();
+
+		//For some reason, the Benchmark runner won't manage to get the mutex whereas individual execution doesn't cause issues
+		//using (Py.GIL())
+		//{
+		// create a Python scope
+		using (PyModule scope = Py.CreateScope())
+		{
+			// convert the Person object to a PyObject
+			PyObject pySudoku = s.Cells.ToPython();
+
+			// create a Python variable "person"
+			scope.Set("sudoku", pySudoku);
+
+			// the person object may now be used in Python
+            String code = System.IO.File.ReadAllText("SolveurNeuronnePython.py");
+			scope.Exec(code);
+            var result = scope.Get("result").As<int[,]>();
+			return new SudokuGrid() { Cells = result };
+		}
+		//}
+
+	}
 
 }
