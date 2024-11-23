4744d6bc6c19bb85bd61651b45906704b4e153ba
kitch naomi <91596994+kthnao@users.noreply.github.com>
2024-11-23 19:56:50 +0200

    pb avec import aima3

--- SolverCSPAIMA/SolverNaoCSPAIMA.cs
+++ SolverCSPAIMA/SolverNaoCSPAIMA.cs
@@ -1,18 +1,33 @@
 using Sudoku.Shared;
+using Python.Runtime;
 
-namespace Sudoku.SolverCSPAIMA{
-
-public class SolverNaoCSPAIMA: ISudokuSolver
+namespace Sudoku.SolverCSPAIMA
 {
-    public SudokuGrid Solve(SudokuGrid s)
+    public class SolverNaoCSPAIMA : ISudokuSolver
+    {
+        public SudokuGrid Solve(SudokuGrid s)
         {
-            return s.CloneSudoku();
+            using (Py.GIL())
+            {
+                dynamic pySolver = Py.Import("aima_csp_solver");
+                var pyResult = pySolver.solve_sudoku(s.Cells);
+
+                int[,] result = ConvertPyObjectTo2DArray(pyResult);
+                return new SudokuGrid() { Cells = result };
+            }
         }
 
+        private int[,] ConvertPyObjectTo2DArray(dynamic pyResult)
+        {
+            int[,] result = new int[9, 9];
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    result[i, j] = (int)pyResult[i][j];
+                }
+            }
+            return result;
+        }
+    }
 }
-}
-
-
-
-    
-
