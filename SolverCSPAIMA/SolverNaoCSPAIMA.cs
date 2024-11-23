9c89285b9c984cb0c11f54da0efd336342919fbe
kitch naomi <91596994+kthnao@users.noreply.github.com>
2024-11-23 21:00:25 +0200

    reparation du bench

--- SolverCSPAIMA/SolverNaoCSPAIMA.cs
+++ SolverCSPAIMA/SolverNaoCSPAIMA.cs
@@ -1,33 +1,19 @@
 using Sudoku.Shared;
 using Python.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Sudoku.SolverCSPAIMA
 {
     public class SolverNaoCSPAIMA : ISudokuSolver
     {
-        public SudokuGrid Solve(SudokuGrid s)
+     
+		 public SudokuGrid Solve(SudokuGrid s)
         {
-            using (Py.GIL())
-            {
-                dynamic pySolver = Py.Import("aima_csp_solver");
-                var pyResult = pySolver.solve_sudoku(s.Cells);
-
-                int[,] result = ConvertPyObjectTo2DArray(pyResult);
-                return new SudokuGrid() { Cells = result };
-            }
-        }
-
-        private int[,] ConvertPyObjectTo2DArray(dynamic pyResult)
-        {
-            int[,] result = new int[9, 9];
-            for (int i = 0; i < 9; i++)
-            {
-                for (int j = 0; j < 9; j++)
-                {
-                    result[i, j] = (int)pyResult[i][j];
-                }
-            }
-            return result;
+            return s.CloneSudoku();
         }
     }
 }
