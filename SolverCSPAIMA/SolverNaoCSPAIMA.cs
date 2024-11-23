c5b1e164c73694513db9a60bcffaff58a31be196
kitch naomi <91596994+kthnao@users.noreply.github.com>
2024-11-23 21:43:15 +0200

    accès fichier ressources

--- SolverCSPAIMA/SolverNaoCSPAIMA.cs
+++ SolverCSPAIMA/SolverNaoCSPAIMA.cs
@@ -8,12 +8,55 @@ using System.Threading.Tasks;
 
 namespace Sudoku.SolverCSPAIMA
 {
-    public class SolverNaoCSPAIMA : ISudokuSolver
+    public class SolverNaoCSPAIMA : PythonSolverBase
+
     {
      
-		 public SudokuGrid Solve(SudokuGrid s)
-        {
-            return s.CloneSudoku();
-        }
-    }
+		public override Shared.SudokuGrid Solve(Shared.SudokuGrid s)
+		{
+			//System.Diagnostics.Debugger.Break();
+
+			//For some reason, the Benchmark runner won't manage to get the mutex whereas individual execution doesn't cause issues
+			//using (Py.GIL())
+			//{
+			// create a Python scope
+			using (PyModule scope = Py.CreateScope())
+			{
+
+				// Injectez le script de conversion
+				AddNumpyConverterScript(scope);
+
+				// Convertissez le tableau .NET en tableau NumPy
+				var pyCells = AsNumpyArray(s.Cells, scope);
+
+				// create a Python variable "instance"
+				scope.Set("instance", pyCells);
+
+				// run the Python script
+				string code = Resources.CSPAIMA_py;
+				scope.Exec(code);
+
+				PyObject result = scope.Get("result");
+
+				// Convertissez le résultat NumPy en tableau .NET
+				var managedResult = AsManagedArray(scope, result);
+
+				return new SudokuGrid() { Cells = managedResult };
+			}
+			//}
+
+		}
+
+		
+
+
+		protected override void InitializePythonComponents()
+		{
+			//declare your pip packages here
+			InstallPipModule("numpy");
+			base.InitializePythonComponents();
+		}
+
+	}
 }
+    
